46216d8ca57184b01318364de7d6375aa916525d
sebraponi <sebraponi@gmail.com>
2019-06-11 10:31:13 +0300

    Initial commit

---
+++ RevitSaveAs/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace SaveFamily
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            string fileName = doc.Title;
+            string filePath = doc.PathName;
+
+            SaveAsOptions options = new SaveAsOptions();      
+            options.OverwriteExistingFile = true;
+            options.Compact = true;
+            
+            doc.SaveAs(filePath, options);
+ 
+            return Result.Succeeded;
+        }
+    }
+}
